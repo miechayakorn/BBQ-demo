f6bc41064e4183eccfb2b27d4582582337f86dd5
Mie Chayakorn <miechayakorn@gmail.com>
2020-02-25 15:05:27 +0700

    [Edit] comment function ยกเลิก and เลื่อนนัด

--- bbq-demo/src/components/CardAppointment.vue
+++ bbq-demo/src/components/CardAppointment.vue
@@ -10,10 +10,10 @@
       </p>
       <div href="#" class="btn btn-primary">{{ item.time }}</div>
     </div>
-    <div class="card-body">
+    <!-- <div class="card-body">
       <a href="#" class="card-link">ยกเลิก</a>
       <a href="#" class="card-link">เลื่อนนัด</a>
-    </div>
+    </div> -->
   </div>
 </template>
 
